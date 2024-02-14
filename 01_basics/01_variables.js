3513da8c7d35a86a4ef6f976a8fcb24a333fac76
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-14 10:02:34 +0000

    variables-learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountid = 123
+let acemail = "vishalm@gmail.com"
+var accpassword ="1234"
+accountcity ="vadodara"
+
+
+// accountid =23 // not allowed
+
+console.log(accountid);
+
+acemail ="vmehta@gmail.com"
+accpassword="432"
+accountcity="mahuva"
+
+console.table([accountid,accpassword,acemail,accountcity])
