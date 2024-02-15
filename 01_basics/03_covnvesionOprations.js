38bdfa3a0e31733d95f18446a02525914963a6ac
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-15 08:42:33 +0000

    conversionof datatypes

---
+++ 01_basics/03_covnvesionOprations.js
@@ -0,0 +1,23 @@
+let score = "33vish"
+
+console.log(typeof score);
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber)
+console.log(valueInnumber);
+
+//"33" => 33
+//"33vish" => NaN => not an number
+// true => 1 ; false => 0
+
+let islogin = 1
+
+
+let booleanisLogin = Boolean(islogin)
+console.log(booleanisLogin);
+
+// 1 => true ; false =>0
+// "" => false
+//"vish" => true
+
+
