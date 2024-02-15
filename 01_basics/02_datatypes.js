1de2a83bde58f25d47fb8662677a7d640f5e99d5
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-15 08:26:44 +0000

    datatype learnng

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "vishal" //string
+let age = 26        //numner => 2 power 53
+let isLogin = true  //boolean
+
+
+//BigInt =>121211212  bignumber
+//null =  => stand alone value , not a empty value
+//undefined => value not assinged
+//symbol => unique
+
+console.log (typeof name)
+
+
+
+
