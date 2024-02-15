9830be89ae5817a5993913db09acf6f881a32fcf
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-15 12:15:28 +0000

    details in datatype

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+console.log(null > 0)
+console.log(null == 0);
+console.log(null >= 0)
+
+
+console.log(undefined > 0)
+console.log(undefined == 0);
+console.log(undefined >= 0)
+
+// === => strict check with datatype
+
+
+console.log("2"==2);
+console.log("2"===2);
+
---
+++ 01_basics/05_datatype-suumury.js
@@ -0,0 +1,42 @@
+// => primitive datatype :
+
+// 7 types = string , Number , Boolean , null , undefined ,symbol ,Bigint
+
+
+let score = 300
+let islogin =true
+let name = "vishal"
+let temp = null
+let email
+
+let id = Symbol(123)
+let newid = Symbol(123)
+
+console.log(id == newid);
+
+
+
+// Non primitive or reference type :
+
+// Array , objects ,Fuctions 
+
+let heros =["bhagatsingh", "ganhiji","boje"]    //Array
+
+let myobj ={                                    //object
+    name:"vishal",
+    city:"vadodara",
+    mobile: 32332232,
+ 
+}
+
+let myfunction = function (){                  // Function
+    console.log( "This is function")
+}
+
+console.log(heros)
+console.log(myobj)
+console.log(myfunction)
+
+
+
+             
