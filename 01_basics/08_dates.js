5a1a645af7f764c25fa9f74e438a8be7b81a3e95
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-16 12:56:50 +0000

    update date function

--- 01_basics/08_dates.js
+++ 01_basics/08_dates.js
@@ -2,6 +2,7 @@
 
 
 let mydate = new Date()
+console.log(mydate.getDate)
 console.log(mydate.toString());
 console.log(mydate.toDateString());
 console.log(mydate.toISOString());
@@ -11,3 +12,9 @@ console.log(typeof mydate);                 // date type of object
 
 let mynewdate = new Date (2024,30,10)
 console.log(mynewdate.toDateString());
+console.log(mydate.getDate())
+console.log(mydate.getMonth())
+
+mydate.toLocaleString('default',{
+    weekday: "long",
+})
