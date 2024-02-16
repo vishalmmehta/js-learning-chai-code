ea9d5a7fe7131baf16004d25ea9da2b380fd3590
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-16 14:19:36 +0000

    learning objectes

---
+++ 02_basics/02_objectes.js
@@ -0,0 +1,18 @@
+// learning objects
+
+let details = {
+    name:"vishal",
+    city:"vadodara",
+    Education:"mca,bca",
+    mobile:9033879982
+
+}
+
+console.log(details);
+
+console.log(details.city);
+
+console.log(details["Education"]);
+
+details.city = "mahuva"
+console.log(details.city);
