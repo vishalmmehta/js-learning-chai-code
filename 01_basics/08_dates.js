a54504d322d4e273619a314953c0e9def88ce42d
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-16 08:21:30 +0000

    learnin date fuctions

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,13 @@
+// Dates 
+
+
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleDateString());
+console.log(typeof mydate);                 // date type of object
+
+
+let mynewdate = new Date (2024,30,10)
+console.log(mynewdate.toDateString());
