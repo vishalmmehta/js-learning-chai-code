cbacbd880fe8e97435be9f0d4267a8a034787491
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-16 08:06:20 +0000

    learning numbr and math

---
+++ 01_basics/07_maths-number.js
@@ -0,0 +1,26 @@
+const score = 200
+
+const newscore = new Number(100)
+
+console.log(score);
+console.log(newscore);
+
+console.log(newscore.toString());
+console.log(newscore.toFixed(2));
+let newnum = 2123.6677
+
+console.log(newnum.toPrecision(4));
+console.log(newnum.toLocaleString('en-IN'));
+
+
+// ***************** Maths *******************
+
+
+let mymath = -22322
+
+console.log(mymath);
+// console.log(Math.ceil(4,2));
+console.log(Math.max[mymath]);
+
+console.log(Math.max(1,2,3,45,5))
+console.log(Math.min(1,2,3,45,5))
