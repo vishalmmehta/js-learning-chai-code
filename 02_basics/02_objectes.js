d823f0ef00b0c661f466af23b3e26473afbef36c
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-17 09:46:53 +0000

    objecte in details

--- 02_basics/02_objectes.js
+++ 02_basics/02_objectes.js
@@ -16,3 +16,48 @@ console.log(details["Education"]);
 
 details.city = "mahuva"
 console.log(details.city);
+
+details.name ="mitesh"
+console.log(details.name);
+details.name= "vishal"
+// Object.freeze(details)          // this method not allowed you to add nee value
+details.name="pratik"
+console.log(details.name);
+
+
+details.greeting = function(){
+    console.log("hello world");
+}
+
+details.greeting2 = function(){
+    console.log(`hello world,${this.name}`);
+}
+console.log(details.greeting());
+console.log(details.greeting2());
+
+
+// --------------- second way to declare object  -----------
+
+let newobj = new Object()
+console.log("This is second way for declare object");
+console.log(newobj);
+
+let userdetails= {
+    id:1,
+    Fullname:{
+        FirstName:"vishal",
+        Lastname:"chudasama",
+    }
+
+}
+
+console.log(userdetails);
+console.log(userdetails.Fullname);
+console.log(userdetails.Fullname.FirstName);
+
+obj1 ={1:"vishal",2:"mehta"}
+obj2= {3:"milan",4:"chudasama"}
+
+obj3= Object.assign({},obj1,obj2)
+console.log(obj3)
+
