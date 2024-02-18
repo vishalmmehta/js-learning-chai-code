77a6a0d117f6360b897b69561c3403acbd4dc551
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-18 08:07:05 +0000

    learnning control flow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,22 @@
+// if statments
+
+if(true){
+
+}
+
+let userlogin = true
+
+if(userlogin){
+    console.log("user loged in");
+}
+
+if(2 == "2"){
+    console.log("This is normal check of datatype");
+}
+
+if(2 === "2"){
+    console.log("This is stricly check of datatype");
+}
+else{
+    console.log("strickly check");
+}
