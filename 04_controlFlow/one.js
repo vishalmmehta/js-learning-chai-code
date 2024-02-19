5791e49fc8a62143f31dcc926a99dff50a06b348
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-19 08:10:15 +0000

    changes

--- 04_controlFlow/one.js
+++ 04_controlFlow/one.js
@@ -10,11 +10,11 @@ if(userlogin){
     console.log("user loged in");
 }
 
-if(2 == "2"){
-    console.log("This is normal check of datatype");
+if(2 == "2"){                                   
+    console.log("This is normal check of datatype");    //== check only condition 
 }
 
-if(2 === "2"){
+if(2 === "2"){                                          //=== check condition as well as it's datatypes 
     console.log("This is stricly check of datatype");
 }
 else{
