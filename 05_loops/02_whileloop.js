7bc78271a5f38733dc24f5be1cf072f9f115f626
vishalshreejii <160007521+vishalshreejii@users.noreply.github.com>
2024-02-19 10:45:36 +0000

    updated code

---
+++ 05_loops/02_whileloop.js
@@ -0,0 +1,13 @@
+
+let i =0
+while (i <= 10) {
+    console.log(`value of i is : ${i}`);
+    i = i + 2;
+}
+
+// do whiile loop
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
